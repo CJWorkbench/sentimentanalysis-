ddb400abf686edd6e8e511e914bcad617b50c08a
jstray <jonathanstray@gmail.com>
2018-05-19 18:46:25 -0700

    Convert to class-less module API

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,41 +1,22 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import numpy as np
-import pandas as pd
 
-class SentimentAnalyser:
+def render(table, params):
 
-    @staticmethod
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-
-        column = wf_module.get_param_column('column_name')
-        if column == '' or column == None:
-            wf_module.set_ready(notify=False)
-            return None
-
-        if column not in table.columns:
-            wf_module.set_error("No column {} exists.".format(column))
-            return None
-
-        all_texts = table[column]
-        sentiment = []
-
-        sid = SentimentIntensityAnalyzer()
-
-        for text in all_texts:
-            if type(text) != str:
-                text = str(text)
-            score = sid.polarity_scores(text)
-            sentiment.append(score['compound'])
-
-        # add column to existing table
-        # table = table.assign(sentiment=pd.Series(np.asarray(sentiment, dtype=np.float32)))
-        table.insert(0, "Sentiment", pd.Series(np.asarray(sentiment, dtype=np.float32)))
+    column = params['column_name']
+    if column == '' or column == None:
         return table
+
+    all_texts = table[column]
+    sentiment = []
+
+    sid = SentimentIntensityAnalyzer()
+
+    for text in all_texts:
+        if type(text) != str:
+            text = str(text)
+        score = sid.polarity_scores(text)
+        sentiment.append(score['compound'])
+
+    # add column to existing table
+    table.insert(0, "Sentiment", pd.Series(np.asarray(sentiment, dtype=np.float32)))
+    return table
